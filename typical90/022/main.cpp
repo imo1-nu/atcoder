f881d93964e9fd2a379d70dc9f48985cd1f13574
ceiver <jagaimo20031129@gmail.com>
2024-08-13 18:22:22 +0900

    典型90, 022まで

--- typical90/022/main.cpp
+++ typical90/022/main.cpp
@@ -3,9 +3,20 @@
 using namespace std;
 using ll = long long;
 
+ll gcd(ll a, ll b)
+{
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
 int main()
 {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    ll A, B, C;
+    cin >> A >> B >> C;
+    ll g = gcd(A, gcd(B, C));
+    A /= g; B /= g; C /= g;
+    cout << A + B + C - 3 << endl;
 }
