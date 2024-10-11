f5a478a393bf38cfafe18b506c9b3efc7413355a
ceiver <jagaimo20031129@gmail.com>
2024-10-11 15:37:59 +0900

    test

--- tessoku-book/a46/main.cpp
+++ tessoku-book/a46/main.cpp
@@ -2,9 +2,70 @@
 using namespace std;
 using ll = long long;
 
+int N;
+vector<int> P;
+vector<double> X, Y;
+
+double dist(int p, int q) {
+    return sqrt((X[p]-X[q])*(X[p]-X[q]) + (Y[p]-Y[q])*(Y[p]-Y[q]));
+}
+
+int RandInt(int l, int r) {
+    return rand() % (r-l+1) + l;
+}
+
+double Randouble(double l, double r) {
+    return (double)rand() / RAND_MAX * (r-l) + l;
+}
+
+double GetScore() {
+    double res = 0;
+    for (int i = 0; i < P.size(); i++) {
+        res += dist(P[i], P[i+1]);
+    }
+    return res;
+}
+
 int main()
 {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    int N;
+    cin >> N;
+
+    P.resize(N+2);
+    for (int i = 1; i <= N; i++) {
+        P[i] = i;
+    }
+    P[N + 1] = 1;
+
+    X.resize(N+1);
+    Y.resize(N+1);
+    for (int i = 1; i <= N; i++) {
+        cin >> X[i] >> Y[i];
+    }
+
+    double CurrentScore = GetScore();
+    for (int i = 0; i < 200000; i++) {
+        int l = RandInt(2, N);
+        int r = RandInt(2, N);
+        if (l > r) {
+            swap(l, r);
+        }
+        reverse(P.begin() + l, P.begin() + r + 1);
+        double NewScore = GetScore();
+
+        double T = 30.00 - 28 * i / 200000;
+        double prob = exp((CurrentScore - NewScore) / T);
+        if (NewScore < CurrentScore || Randouble(0, 1) < prob) {
+            CurrentScore = NewScore;
+        } else {
+            reverse(P.begin() + l, P.begin() + r + 1);
+        }
+    }
+    
+    for (int i = 1; i <= N + 1; i++) {
+        cout << P[i] << endl;
+    }
 }
--- tessoku-book/a49/main.cpp
+++ tessoku-book/a49/main.cpp
@@ -2,9 +2,85 @@
 using namespace std;
 using ll = long long;
 
+#define WIDTH 10000
+
+struct State {
+    int score;
+    int X[20];
+    char LastMove;
+    int LastPos;
+};
+
 int main()
 {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    int T;
+    cin >> T;
+    vector<int> P(T), Q(T), R(T);
+    for (int i = 0; i < T; ++i) {
+        cin >> P[i] >> Q[i] >> R[i];
+        P[i]--; Q[i]--; R[i]--;
+    }
+
+    vector<vector<State>> Beam(T + 1);
+    State s;
+    s.score = 20;
+    for (int i = 0; i < 20; i++) {
+        s.X[i] = 0;
+    }
+    Beam[0].push_back(s);
+
+    for (int i = 0; i < T; i++) {
+        vector<State> Candidate;
+        for (int j = 0; j < Beam[i].size(); j++) {
+            State SousaA = Beam[i][j];
+            SousaA.LastMove = 'A';
+            SousaA.LastPos = j;
+            SousaA.X[P[i]]++;
+            SousaA.X[Q[i]]++;
+            SousaA.X[R[i]]++;
+            for (int k = 0; k < 20; k++) {
+                if (SousaA.X[k] == 0) {
+                    SousaA.score++;
+                }
+            }
+            
+            State SousaB = Beam[i][j];
+            SousaB.LastMove = 'B';
+            SousaB.LastPos = j;
+            SousaB.X[P[i]]++;
+            SousaB.X[Q[i]]++;
+            SousaB.X[R[i]]++;
+            for (int k = 0; k < 20; k++) {
+                if (SousaB.X[k] == 0) {
+                    SousaB.score++;
+                }
+            }
+
+            Candidate.push_back(SousaA);
+            Candidate.push_back(SousaB);
+        }
+
+        sort(Candidate.begin(), Candidate.end(), [](const State &a, const State &b) {
+            return a.score > b.score;
+        });
+        
+        int Num = min(WIDTH, (int)Candidate.size());
+        for (int j = 0; j < Num; j++) {
+            Beam[i + 1].push_back(Candidate[j]);
+        }
+    }
+    
+    int CurrentPos = 0;
+    vector<char> Ans(T + 1); 
+    for (int i = T; i >= 1; i++) {
+        Ans[i] = Beam[i][CurrentPos].LastMove;
+        CurrentPos = Beam[i][CurrentPos].LastPos;
+    }
+    
+    for (int i = 1; i <= T; i++) {
+        cout << Ans[i] << endl;
+    }
 }
