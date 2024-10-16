b718e42be62d8bcb62d3d4f980eb7b44c5f1729c
ceiver <jagaimo20031129@gmail.com>
2024-10-16 22:39:58 +0900

    abc375eまで

--- abc/abc375/d/main.cpp
+++ abc/abc375/d/main.cpp
@@ -10,9 +10,24 @@ int main()
     
     string S;
     cin >> S;
-    vector<vector<int>> A(26);
-    for (int i = 0; i < S.length(); i++) {
-        A[S[i] - 'A']
+    ll N = S.size();
+    vector<vector<ll>> A(26, vector<ll>(N));
+    for (ll i = 0; i < N; i++) {
+        A[S[i] - 'A'][i] = 1;
+    }
+    vector<vector<ll>> B(26, vector<ll>(N + 1));
+    for (ll i = 0; i < 26; i++) {
+        for (ll j = 1; j <= N; j++) {
+            B[i][j] = B[i][j - 1] + A[i][j - 1];
+        }
     }
     
+    ll ans = 0;
+    for (ll i = 0; i < N; i++) {
+        for (ll k = 0; k < 26; k++) {
+            ans += B[k][i] * (B[k][N] - B[k][i + 1]);
+        }
+    }
+    
+    cout << ans << endl;
 }
--- abc/abc375/e/main.cpp
+++ abc/abc375/e/main.cpp
@@ -8,4 +8,51 @@ int main()
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i] >> B[i];
+    }
+
+    vector<int> S(N + 1);
+    for (int i = 1; i <= N; i++) {
+        S[i] = S[i - 1] + B[i - 1];
+    }
+
+    vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(S[N] / 3 + 1, vector<int>(S[N] / 3 + 1, 1e9)));
+    dp[0][0][0] = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= S[N] / 3; j++) {
+            for (int k = 0; k <= S[N] / 3; k++) {
+                if (j - B[i - 1] >= 0) {
+                    if (A[i - 1] == 1) {
+                        dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j - B[i - 1]][k]);
+                    } else {
+                        dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j - B[i - 1]][k] + 1);
+                    }
+                }
+
+                if (k - B[i - 1] >= 0) {
+                    if (A[i - 1] == 2) {
+                        dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k - B[i - 1]]);
+                    } else {
+                        dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k - B[i - 1]] + 1);
+                    }
+                }
+
+                if (A[i - 1] == 3) {
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k]);
+                } else {
+                    dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k] + 1);
+                }
+            }
+        }
+    }
+    
+    if (S[N] % 3 == 0 && dp[N][S[N] / 3][S[N] / 3] < 1e9) {
+        cout << dp[N][S[N] / 3][S[N] / 3] << endl;
+    } else {
+        cout << -1 << endl;
+    }
 }
