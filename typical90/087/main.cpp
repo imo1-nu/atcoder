eff04a8db18290d517ec4324ff04541b704d2e08
ceiver <jagaimo20031129@gmail.com>
2024-11-09 13:19:37 +0900

    典型087まで

--- typical90/087/main.cpp
+++ typical90/087/main.cpp
@@ -3,9 +3,112 @@
 using namespace std;
 using ll = long long;
 
+#define INF LLONG_MAX / 2
+
 int main()
 {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    ll N, P, K;
+    cin >> N >> P >> K;
+    vector<vector<ll>> A(N, vector<ll>(N));
+    for (ll i = 0; i < N; i++) {
+        for (ll j = 0; j < N; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    ll L = 0, R = INF;
+    ll l = 0, r = INF;
+    while (l + 1 < r) {
+        ll m = (l + r) / 2;
+        vector<vector<ll>> dist(N, vector<ll>(N, INF));
+        for (ll i = 0; i < N; i++) {
+            for (ll j = 0; j < N; j++) {
+                if (A[i][j] != -1) {
+                    dist[i][j] = A[i][j];
+                }
+                else {
+                    dist[i][j] = m;
+                }
+            }
+        }
+
+        for (ll k = 0; k < N; k++) {
+            for (ll k2 = 0; k2 < N; k2++) {
+                for (ll k3 = 0; k3 < N; k3++) {
+                    dist[k2][k3] = min(dist[k2][k3], dist[k2][k] + dist[k][k3]);
+                }
+            }
+        }
+
+        ll cnt = 0;
+        for (ll k = 0; k < N; k++) {
+            for (ll k2 = k + 1; k2 < N; k2++) {
+                if (dist[k][k2] <= P) {
+                    cnt++;
+                }
+            }
+        }
+
+        if (cnt < K) {
+            r = m;
+        }
+        else {
+            l = m;
+        }
+    }
+
+    R = r;
+    
+
+    l = 0, r = INF;
+    while (l + 1 < r) {
+        ll m = (l + r) / 2;
+        vector<vector<ll>> dist(N, vector<ll>(N, INF));
+        for (ll i = 0; i < N; i++) {
+            for (ll j = 0; j < N; j++) {
+                if (A[i][j] != -1) {
+                    dist[i][j] = A[i][j];
+                }
+                else {
+                    dist[i][j] = m;
+                }
+            }
+        }
+
+        for (ll k = 0; k < N; k++) {
+            for (ll k2 = 0; k2 < N; k2++) {
+                for (ll k3 = 0; k3 < N; k3++) {
+                    dist[k2][k3] = min(dist[k2][k3], dist[k2][k] + dist[k][k3]);
+                }
+            }
+        }
+
+        ll cnt = 0;
+        for (ll k = 0; k < N; k++) {
+            for (ll k2 = k + 1; k2 < N; k2++) {
+                if (dist[k][k2] <= P) {
+                    cnt++;
+                }
+            }
+        }
+
+        if (cnt > K) {
+            l = m;
+        }
+        else {
+            r = m;
+        }
+    }
+
+    L = l;
+
+    if (R == INF && r != INF) {
+        cout << "Infinity" << endl;
+    }
+    else {
+        cout << R - L - 1 << endl;
+    }
 }
--- typical90/088/main.cpp
+++ typical90/088/main.cpp
@@ -3,9 +3,53 @@
 using namespace std;
 using ll = long long;
 
+ll N, Q;
+ll A[88], X[4000], Y[4000];
+vector<ll> G[88];
+bool used[88];
+bool counged[8889];
+
+void dfs(ll u, ll sum) {
+    if (u == N) {
+        if (sum == 0) return;
+        if (counged[sum] == false) {
+            counged[sum] = true;
+        }
+        else {
+            cout << "No" << endl;
+            exit(0);
+        }
+    }
+
+    if (used[u] == false) {
+        sum += A[u];
+        for (ll v : G[u]) {
+            used[v] = true;
+        }
+        dfs(u + 1, sum);
+        sum -= A[u];
+        for (ll v : G[u]) {
+            used[v] = false;
+        }
+    }
+
+    dfs(u + 1, sum);
+}
+
 int main()
 {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    cin >> N >> Q;
+    for (ll i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    for (ll i = 0; i < Q; i++) {
+        cin >> X[i] >> Y[i];
+        X[i]--; Y[i]--;
+        G[X[i]].push_back(Y[i]);
+    }
+
 }
