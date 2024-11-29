dca8d323d99808c6d20645c6fcca73a3998400f5
imo1-nu <jagaimo20031129@gmail.com>
2024-11-29 15:14:48 +0900

    abc189/e修正

--- abc/abc189/e/main.cpp
+++ abc/abc189/e/main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 using ll = long long;
 
-vector<vector<int>> mat_mal(vector<vector<int>> &A, vector<vector<int>> &B) {
-    int N = A.size();
-    vector<vector<int>> C(N, vector<int>(N));
-    for (int i = 0; i < N; i++) {
-        for (int k = 0; k < N; k++) {
-            for (int j = 0; j < N; j++) {
+vector<vector<ll>> mat_mal(vector<vector<ll>> &A, vector<vector<ll>> &B) {
+    ll N = A.size();
+    vector<vector<ll>> C(N, vector<ll>(N));
+    for (ll i = 0; i < N; i++) {
+        for (ll k = 0; k < N; k++) {
+            for (ll j = 0; j < N; j++) {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
@@ -16,11 +16,11 @@ vector<vector<int>> mat_mal(vector<vector<int>> &A, vector<vector<int>> &B) {
     return C;
 }
 
-vector<int> mat_vec_mal(vector<vector<int>> &A, vector<int> &B) {
-    int N = A.size();
-    vector<int> C(N);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+vector<ll> mat_vec_mal(vector<vector<ll>> &A, vector<ll> &B) {
+    ll N = A.size();
+    vector<ll> C(N);
+    for (ll i = 0; i < N; i++) {
+        for (ll j = 0; j < N; j++) {
             C[i] += A[i][j] * B[j];
         }
     }
@@ -32,59 +32,63 @@ int main()
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
-    int N;
+    ll N;
     cin >> N;
-    vector<ll> X(N), Y(N);
-    for (int i = 0; i < N; i++) {
-        cin >> X[i] >> Y[i];
+    vector<vector<ll>> P(N, vector<ll>(3));
+    for (ll i = 0; i < N; i++) {
+        cin >> P[i][0] >> P[i][1];
+        P[i][2] = 1;
     }
-    int M;
+
+    ll M;
     cin >> M;
-    vector<int> op(M), p(M);
-    for (int i = 0; i < M; i++) {
+    vector<ll> op(M), p(M);
+    for (ll i = 0; i < M; i++) {
         cin >> op[i];
         if (op[i] == 3 || op[i] == 4) {
             cin >> p[i];
         }
     }
 
-    vector<vector<int>> rot1 = {{0, -1}, {1, 0}};
-    vector<vector<int>> rot2 = {{0, 1}, {-1, 0}};
-    vector<vector<int>> move3 = {{-1, 0}, {0, 1}};
-    vector<vector<int>> move4 = {{1, 0}, {0, -1}};
-    vector<vector<vector<int>>> mat(M + 1, vector<vector<int>>(2, vector<int>(2)));
-    vector<vector<int>> plus(M + 1, vector<int>(2));
-    mat[0] = {{1, 0}, {0, 1}};
-    plus[0] = {0, 0};
-    for (int i = 0; i < M; i++) {
-        if (op[i] == 1) {
-            mat[i + 1] = mat_mal(rot1, mat[i]);
-            plus[i + 1] = mat_vec_mal(rot1, plus[i]);
-        }
-        else if (op[i] == 2) {
-            mat[i + 1] = mat_mal(rot2, mat[i]);
-            plus[i + 1] = mat_vec_mal(rot2, plus[i]);
-        }
-        else if (op[i] == 3) {
-            mat[i + 1] = mat_mal(move3, mat[i]);
-            plus[i + 1] = mat_vec_mal(move3, plus[i]);
-            plus[i + 1][0] -= 2 * p[i];
-        }
-        else {
-            mat[i + 1] = mat_mal(move4, mat[i]);
-            plus[i + 1] = mat_vec_mal(move4, plus[i]);
-            plus[i + 1][1] -= 2 * p[i];
-        }
+    vector<vector<vector<ll>>> S(M + 1, vector<vector<ll>>(3, vector<ll>(3)));
+    for (ll i = 0; i < 3; i++) {
+        S[0][i][i] = 1;
     }
 
-    int Q;
+    for (int i = 0; i < M; i++) {
+        vector<vector<ll>> A(3, vector<ll>(3));
+        if (op[i] == 1) {
+            A[0][1] = 1;
+            A[1][0] = -1;
+            A[2][2] = 1;
+        }
+        if (op[i] == 2) {
+            A[0][1] = -1;
+            A[1][0] = 1;
+            A[2][2] = 1;
+        }
+        if (op[i] == 3) {
+            A[0][0] = -1;
+            A[1][1] = 1;
+            A[2][2] = 1;
+            A[0][2] = 2 * p[i];
+        }
+        if (op[i] == 4) {
+            A[0][0] = 1;
+            A[1][1] = -1;
+            A[2][2] = 1;
+            A[1][2] = 2 * p[i];
+        }
+        S[i + 1] = mat_mal(A, S[i]);
+    }
+
+    ll Q;
+    cin >> Q;
     while (Q--) {
-        int A, B;
+        ll A, B;
         cin >> A >> B;
         B--;
-        vector<int> ans = mat_vec_mal(mat[A], {X[B], Y[B]});
-        ans[0] += plus[A][0];
-        ans[1] += plus[A][1];
+        vector<ll> ans = mat_vec_mal(S[A], P[B]);
         cout << ans[0] << " " << ans[1] << endl;
     }
 }
