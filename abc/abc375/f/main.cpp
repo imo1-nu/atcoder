873bd650e9e2cbf331b0eb7259ce31746cc6e76e
imo1-nu <jagaimo20031129@gmail.com>
2024-12-27 23:39:30 +0900

    abc過去問

--- abc/abc375/f/main.cpp
+++ abc/abc375/f/main.cpp
@@ -8,4 +8,74 @@ int main()
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    int N, M, Q;
+    cin >> N >> M >> Q;
+    vector<tuple<ll, int, int>> R(M);
+    for (int i = 0; i < M; i++) {
+        int A, B, C;
+        cin >> A >> B >> C;
+        R[i] = {C, A - 1, B - 1};
+    }
+
+    vector<tuple<int, int, int>> query;
+    vector<bool> used(M, true);
+    for (int i = 0; i < Q; i++) {
+        int t, x, y;
+        cin >> t >> x;
+        x--;
+        if (t == 1) {
+            query.push_back({t, x, -1});
+            used[x] = false;
+        }
+        else {
+            cin >> y;
+            y--;
+            query.push_back({t, x, y});
+        }
+    }
+
+    vector<vector<ll>> dist(N, vector<ll>(N, 1e18));
+    for (int i = 0; i < N; i++) {
+        dist[i][i] = 0;
+    }
+    for (int i = 0; i < M; i++) {
+        if (used[i]) {
+            auto [C, A, B] = R[i];
+            dist[A][B] = dist[B][A] = C;
+        }
+    }
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    vector<ll> ans;
+    while (Q--) {
+        auto [t, x, y] = query[Q];
+        if (t == 1) {
+            auto [C, A, B] = R[x];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    dist[i][j] = min(dist[i][j], dist[i][A] + C + dist[B][j]);
+                    dist[i][j] = min(dist[i][j], dist[i][B] + C + dist[A][j]);
+                }
+            }
+        }
+        else {
+            ll d = dist[x][y];
+            if (d == 1e18) {
+                ans.push_back(-1);
+            }
+            else {
+                ans.push_back(d);
+            }
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    for (auto a : ans) {
+        cout << a << endl;
+    }
 }
--- abc/abc377/e/main.cpp
+++ abc/abc377/e/main.cpp
@@ -3,41 +3,42 @@
 using namespace std;
 using ll = long long;
 
-vector<vector<ll>> dp(61, vector<ll> (200009, 0));
-
-ll ans(ll x, ll y) {
-    if (y == 0) return x;
-    ll i;
-    for (i = 61; (1 << i) > y; i--) {}
-    return ans(dp[i][x], y - (1 << i));
-}
-
 int main()
 {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
-    ll n, q;
-    cin >> n >> q;
-    vector<ll> a(n);
-    for (ll i = 0; i < n; i++) {
-        cin >> a[i];
+    ll N, K;
+    cin >> N >> K;
+    vector<int> P(N);
+    for (int i = 0; i < N; i++) {
+        cin >> P[i];
+        P[i]--;
     }
 
-    for (ll i = 1; i <= n; i++) {
-        dp[0][i] = a[i - 1];
+    vector<int> ans(N);
+    vector<vector<int>> group;
+    vector<bool> used(N);
+    for (int i = 0; i < N; i++) {
+        if (used[i]) continue;
+        vector<int> g;
+        for (int j = i; !used[j]; j = P[j]) {
+            g.push_back(j);
+            used[j] = true;
+        }
+        group.push_back(g);
     }
 
-    for (ll i = 1; i < 60; i++) {
-        for (ll j = 1; j <= n; j++) {
-            dp[i][j] = dp[i - 1][dp[i - 1][j]];
+    for (auto g : group) {
+        int sz = g.size();
+        int shift = atcoder::pow_mod(2, K, sz);
+        for (int i = 0; i < sz; i++) {
+            ans[g[i]] = g[(i + shift) % sz];
         }
     }
-    
-    // q回操作後の配列を出力
-    for (ll j = 1; j <= n; j++) {
-        cout << ans(j, q);
-        if (j != n) cout << " ";
+
+    for (int i = 0; i < N; i++) {
+        cout << ans[i] + 1 << ' ';
     }
     cout << endl;
 }
