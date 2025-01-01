97237c1c6a82e768639a1a6ffabfe94de32bd2ee
imo1-nu <jagaimo20031129@gmail.com>
2025-01-02 03:38:59 +0900

    abc過去問

--- abc/abc349/e/main.cpp
+++ abc/abc349/e/main.cpp
@@ -7,4 +7,64 @@ int main()
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    vector<vector<int>> A(3, vector<int>(3));
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cin >> A[i][j];
+        }
+    }
+
+    auto check = [&](set<int> s) {
+        if (s.count(0) && s.count(1) && s.count(2)) return true;
+        if (s.count(3) && s.count(4) && s.count(5)) return true;
+        if (s.count(6) && s.count(7) && s.count(8)) return true;
+        if (s.count(0) && s.count(3) && s.count(6)) return true;
+        if (s.count(1) && s.count(4) && s.count(7)) return true;
+        if (s.count(2) && s.count(5) && s.count(8)) return true;
+        if (s.count(0) && s.count(4) && s.count(8)) return true;
+        if (s.count(2) && s.count(4) && s.count(6)) return true;
+        return false;
+    };
+
+    function<bool(set<int>, set<int>)> dfs = [&](set<int> s, set<int> t) -> bool {
+        if (check(s)) return true;
+        if (check(t)) return false;
+        if (s.size() == 5) {
+            ll sumA = 0, sumB = 0;
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    if (s.count(i * 3 + j)) sumA += A[i][j];
+                    else sumB += A[i][j];
+                }
+            }
+            return sumA > sumB;
+        }
+
+        bool res;
+        if (s.size() == t.size()) {
+            res = false;
+            for (int i = 0; i < 9; ++i) {
+                if (!s.count(i) && !t.count(i)) {
+                    set<int> s2 = s;
+                    s2.insert(i);
+                    res |= dfs(s2, t);
+                }
+            }
+        }
+        else {
+            res = true;
+            for (int i = 0; i < 9; ++i) {
+                if (!s.count(i) && !t.count(i)) {
+                    set<int> t2 = t;
+                    t2.insert(i);
+                    res &= dfs(s, t2);
+                }
+            }
+        }
+        return res;
+    };
+
+    set<int> s, t;
+    if (dfs(s, t)) cout << "Takahashi" << endl;
+    else cout << "Aoki" << endl;
 }
--- abc/abc350/e/main.cpp
+++ abc/abc350/e/main.cpp
@@ -7,4 +7,16 @@ int main()
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    ll N, A, X, Y;
+    cin >> N >> A >> X >> Y;
+
+    map<ll, double> mp;
+    function<double(ll)> f = [&](ll n) -> double {
+        if (n == 0) return 0;
+        if (mp.count(n)) return mp[n];
+        mp[n] = min(X + f(n / A), (6 * Y + f(n / 2) + f(n / 3) + f(n / 4) + f(n / 5) + f(n / 6)) / 5);
+        return mp[n];
+    };
+
+    cout << fixed << setprecision(10) << f(N) << endl;
 }
