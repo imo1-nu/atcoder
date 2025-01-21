4f8e1388c1e44121b84eb9090f55942dd21c1d27
imo1-nu <jagaimo20031129@gmail.com>
2025-01-21 11:35:59 +0900

    abc過去問

--- abc/abc210/e/main.cpp
+++ abc/abc210/e/main.cpp
@@ -3,9 +3,30 @@
 using namespace std;
 using ll = long long;
 
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
 int main()
 {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     
+    int N, M;
+    cin >> N >> M;
+    vector<pair<ll, int>> P(M);
+    for (int i = 0; i < M; i++) {
+        cin >> P[i].second >> P[i].first;
+    }
+    sort(P.begin(), P.end());
+    int g_sum = N;
+    ll ans = 0;
+    for (int i = 0; i < M; i++) {
+        int d = g_sum - gcd(g_sum, P[i].second);
+        ans += P[i].first * d;
+        g_sum = gcd(g_sum, P[i].second);
+    }
+    if (g_sum == 1) cout << ans << endl;
+    else cout << -1 << endl;
 }
