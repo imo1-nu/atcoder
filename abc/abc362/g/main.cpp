d1c00e5a6891cbea32e80a0e05fa25d281a7ad29
imo1-nu <jagaimo20031129@gmail.com>
2025-02-14 23:36:49 +0900

    abc過去問

--- abc/abc362/g/main.cpp
+++ abc/abc362/g/main.cpp
@@ -3,9 +3,31 @@
 using namespace std;
 using ll = long long;
 
-int main()
-{
+int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
-    
+
+    string S;
+    int Q;
+    cin >> S >> Q;
+    auto SA = atcoder::suffix_array(S);
+
+    while (Q--) {
+        string T;
+        cin >> T;
+
+        auto compare = [&](int i) {
+            return S.compare(i, T.size(), T);
+        };
+
+        auto l = lower_bound(SA.begin(), SA.end(), T, [&](int i, const string &T) {
+            return compare(i) < 0;
+        });
+
+        auto r = lower_bound(SA.begin(), SA.end(), T, [&](int i, const string &T) {
+            return compare(i) <= 0;
+        });
+
+        cout << r - l << endl;
+    }
 }
